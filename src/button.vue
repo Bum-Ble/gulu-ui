3581528f2975f31f24c1b47e5a8589b5a1c96219
Bumble <472302877@qq.com>
2022-04-19 13:46:46 +0800

    添加icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,22 @@
 <template>
-  <button class="g-button">按钮</button>
+<!--  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">-->
+  <button class="g-button" :class=`icon-${iconPosition}`>
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {}
+export default {
+  props:['icon', 'iconPosition']
+}
+
 </script>
 <style lang="scss">
+
 .g-button{
   font-size: var(--font-size);
   height: var(--button-height);
@@ -12,6 +24,10 @@ export default {}
   border: 1px solid var(--border-color);
   border-radius: var(--border-radius);
   background: var(--button-bg);
+  vertical-align: middle;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
   &:hover{
     border-color: var(--border-color-hover);
   }
@@ -20,6 +36,14 @@ export default {}
   }
   &:focus{
     outline: none;
+  }
+  &.icon-right{
+    > .content { order: 1}
+    > .icon {order: 2; margin-left: .2em}
+  }
+  &.icon-left{
+    > .icon{order: 1; margin-right: .2em}
+    > .content{order: 2}
   }
 }
 </style>
