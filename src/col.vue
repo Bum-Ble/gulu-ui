09b1d3d67b5f8aba7fbe9baf2b24a43edfefe2cf
Bumble <472302877@qq.com>
2022-05-12 16:47:19 +0800

    row 和 col 雏形完成

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    span:{
+      type: [String, Number]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.col{
+  height: 80px;
+  background: grey;
+  width: 50%;
+  border: 2px solid green;
+  $class-prefix: col-;
+  @for $n from 1 through 24{
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+}
+</style>
