806748606a6bda765f2bbdffc9b015f3e9c5849e
Bumble <472302877@qq.com>
2022-05-18 16:26:17 +0800

    增加布局组件

---
+++ src/layout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layout" :class="layOutClass">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name:'GuluLayout',
+  data(){
+    return {
+      layOutClass: {
+        hasSider: false
+      }
+    }
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      if (vm.$options.name === 'GuluSider'){
+        this.layOutClass.hasSider = true
+      }
+    })
+  }
+}
+</script>
+<style scoped lang="scss">
+.layout{
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
+  &.hasSider{
+    flex-direction: row;
+  }
+}
+</style>
