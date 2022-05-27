f1389bf62cee04aebdf8bc26045e60078e146954
Bumble <472302877@qq.com>
2022-05-27 15:23:54 +0800

    解决 只能出现一个toast 的bug（会调用两次close方法）

--- src/plugin.js
+++ src/plugin.js
@@ -7,16 +7,24 @@ export default {
       if (currentToast){
         currentToast.close()
       }
-      currentToast = createToast({Vue, propsData:toastOptions, message})
+      currentToast = createToast({
+        Vue,
+        propsData:toastOptions,
+        message,
+        onBeforeClose : () => {
+          currentToast = null
+        }
+      })
     }
   }
 }
 
-function createToast({Vue, propsData, message}){
+function createToast({Vue, propsData, message, onBeforeClose}){
   let Constructor = Vue.extend(Toast)
   let toast = new Constructor({ propsData })
   toast.$slots.default = [message]
   toast.$mount()
+  toast.$on('beforeClose', onBeforeClose)
   document.body.appendChild(toast.$el)
   return toast
 }
--- src/toast.vue
+++ src/toast.vue
@@ -73,6 +73,7 @@ export default {
     },
     close(){
       this.$el.remove()
+      this.$emit('beforeClose')
       this.$destroy()
     },
     onClickClose(){
