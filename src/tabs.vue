04cbadad7d5074764d413f96108f195f991264ad
Bumble <472302877@qq.com>
2022-06-02 11:23:57 +0800

    添加 tabs 相关组件，添加基本 props

---
+++ src/tabs.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tabs">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'GuluTabs',
+  props:{
+    selected:{
+      type: String,
+      required: true
+    },
+    direction:{
+      type: String,
+      default: 'horizontal',
+      validator(val){
+        return ['horizontal', 'vertical'].indexOf(val) >= 0
+      }
+    }
+  },
+  created() {
+    // this.$emit('update:selected',args)
+  }
+}
+</script>
+<style>
+
+</style>
