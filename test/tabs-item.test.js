2500e69acdf4334f771133c5728925e87d890634
Bumble <472302877@qq.com>
2022-06-09 16:39:49 +0800

    tabs 和 tabs-item 的测试

---
+++ test/tabs-item.test.js
@@ -0,0 +1,53 @@
+import Toast from "../src/toast";
+
+const expect = chai.expect;
+import Vue from 'vue'
+import Tabs from '../src/tabs'
+import TabsHead from "../src/tabs-head";
+import TabsItem from "../src/tabs-item";
+import TabsBody from "../src/tabs-body";
+import TabsPane from "../src/tabs-pane";
+import sinon from "sinon/pkg/sinon-esm";
+
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-head', TabsHead)
+Vue.component('g-tabs-item', TabsItem)
+Vue.component('g-tabs-body', TabsBody)
+Vue.component('g-tabs-pane', TabsPane)
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('TabsItem', () => {
+
+  it('存在', () => {
+    expect(TabsItem).to.exist
+  })
+  it('接收 name', () => {
+    const Constructor = Vue.extend(TabsItem)
+    const vm = new Constructor({
+      propsData: {
+        name: 'xxx'
+      }
+    }).$mount()
+    expect(vm.$el.getAttribute('data-name')).to.eq('xxx')
+  })
+
+  it('接收disabled', () => {
+    const Constructor = Vue.extend(TabsItem)
+    const vm = new Constructor({
+      propsData: {
+        name: 'xxx2',
+        disabled: true
+      }
+    }).$mount()
+    expect(vm.$el.classList.contains('disabled')).to.eq(true)
+    const callback = sinon.fake()
+    vm.$on('click', callback)
+    vm.$el.click()
+    expect(callback).to.have.not.been.called
+
+  })
+
+
+})
