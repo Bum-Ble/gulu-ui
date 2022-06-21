15734423a5c673a2d9674798e9d4d8c7c44a9f1e
Bumble <472302877@qq.com>
2022-06-21 09:21:11 +0800

    新增 popover 组件

---
+++ src/popover.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="popover" ref="popover" @click="onClick">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+      <slot name="content"></slot>
+    </div>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name:'GuluPopover',
+  data(){
+    return{
+      visible: false,
+    }
+  },
+  methods:{
+    positionContent(){
+      document.body.appendChild(this.$refs.contentWrapper)
+      let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+    },
+    onClickDocument(e){
+      // console.log(this.$refs.popover,'----------refs')
+      // console.log(e.target,"---------target")
+      if (this.$refs.popover === e.target || this.$refs.popover.contains(e.target)){
+        // console.log("222222")
+        return
+      }
+      this.close()
+    },
+    open(){
+      this.visible = true
+      this.$nextTick(() => {
+        this.positionContent()
+        document.addEventListener('click', this.onClickDocument)
+      })
+    },
+    close(){
+      // console.log("333333")
+      this.visible = false
+      document.removeEventListener('click', this.onClickDocument)
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)){
+        if (this.visible === true){
+          this.close()
+        }else{
+          this.open()
+        }
+      }
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.popover{
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+}
+.content-wrapper{
+  position: absolute;
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0,0,0,0.5) ;
+  transform: translateY(-100%);
+}
+
+</style>
