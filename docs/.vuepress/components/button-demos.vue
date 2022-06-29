b4195dd20e73b432b94712eb36e2767f5422f824
Bumble <472302877@qq.com>
2022-06-29 15:05:10 +0800

    button-demos 增加 button-group的展示

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
     <div style="margin: 10px 0;">
+      <h3>基本用法</h3>
       <g-button>默认按钮</g-button>
       <g-button icon="setting">默认按钮</g-button>
       <g-button :loading="true">默认按钮</g-button>
       <g-button disabled>默认按钮</g-button>
     </div>
-    <pre><code>{{content}}</code></pre>
+    <pre><code>{{content.trim()}}</code></pre>
+    <div>
+      <h3>组合按钮</h3>
+      <g-button-group>
+        <g-button icon="left">上一页</g-button>
+        <g-button>更多</g-button>
+        <g-button icon="right" icon-position="right">下一页</g-button>
+      </g-button-group>
+      <pre><code>{{content2.trim()}}</code></pre>
+    </div>
+
   </div>
 </template>
 <script>
 import Button from '../../../src/button.vue'
+import gButtonGroup from '../../../src/button-group'
 export default {
   name: "input-demos",
   components:{
     'g-button': Button,
+    gButtonGroup
   },
   data(){
     return{
@@ -23,7 +36,14 @@ export default {
 <g-button icon="setting">默认按钮</g-button>
 <g-button :loading="true">默认按钮</g-button>
 <g-button disabled>默认按钮</g-button>
-      `.trim()
+      `,
+      content2:`
+<g-button-group>
+  <g-button icon="left">上一页</g-button>
+  <g-button>更多</g-button>
+  <g-button icon="right" icon-position="right">下一页</g-button>
+</g-button-group>
+      `
     }
   }
 }
