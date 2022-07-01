f57e91e6005a4be445da6389011e8aeda86389a9
Bumble <472302877@qq.com>
2022-07-01 14:33:36 +0800

    创建 cascader 组件

---
+++ src/cascader-item.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="cascaderItem">
+    {{sourceItem.name}}
+    <gulu-cascader-item
+      v-if="sourceItem.children"
+      v-for="item in sourceItem.children"
+      :source-item="item"
+    >
+    </gulu-cascader-item>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "guluCascaderItem",
+  props:{
+    sourceItem:{
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.cascaderItem{
+  border: 1px solid red;
+  margin: 10px;
+}
+
+</style>
---
+++ src/cascader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="cascader">
+    <div class="trigger">
+      <slot></slot>
+    </div>
+    <div class="popover">
+      <div v-if="source" v-for="sourceItem in source">
+        <g-cascader-item :source-item="sourceItem"></g-cascader-item>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import gCascaderItem from "./cascader-item";
+export default {
+  name: "GuluCascader",
+  components:{
+    gCascaderItem
+  },
+  props:{
+    source: {
+      type: Array
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
