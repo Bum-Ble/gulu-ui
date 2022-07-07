10b9faa0b1a047645cb0c9be19acb95284f90312
Bumble <472302877@qq.com>
2022-07-07 10:12:23 +0800

    改用 vue-cli 构建项目

---
+++ src/demo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <g-button>你好</g-button>
+    <g-cascader :source="source" height="200"></g-cascader>
+  </div>
+</template>
+
+<script>
+import gButton from './button'
+import gCascader from "@/cascader";
+
+export default {
+  name: "demo",
+  components: {
+    gButton, gCascader
+  },
+  data() {
+    return {
+      source: [
+        {
+          name: '浙江',
+          children: [
+            {
+              name: '杭州',
+              children: [{name: '上城'}, {name: '下城'}, {name: '江干'},]
+            },
+            {
+              name: '嘉兴',
+              children: [{name: '南湖'}, {name: '秀洲'}, {name: '嘉善'},]
+            }
+          ]
+        },
+        {
+          name: '福建',
+          children: [
+            {
+              name: '福州',
+              children: [{name: '鼓楼'}, {name: '台江'}, {name: '台山'},]
+            }
+          ]
+        },
+        {
+          name: '合肥',
+          children: [{name: '瑶海'}, {name: '庐阳'}]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+*{ margin: 0; padding: 0}
+*, *::before, *::after{box-sizing: border-box}
+</style>
