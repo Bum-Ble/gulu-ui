ef1df9da5921d0a71ba9327aed9de66610e502c8
Bumble <472302877@qq.com>
2022-08-02 10:45:07 +0800

    初步实现轮播图 slides

--- src/demo.vue
+++ src/demo.vue
@@ -1,101 +1,47 @@
 <template>
   <div>
-    <g-button>你好</g-button>
-    <div style="padding: 20px;">
-      <g-cascader :source.sync="source" height="200" :selected.sync="selected" :load-data="loadData"></g-cascader>
-    </div>
-    <div style="padding: 20px;">
-      <g-cascader :source.sync="source" height="200" :selected.sync="selected" :load-data="loadData"></g-cascader>
-    </div>
-
+    <g-slides class="wrapper" width="300px" height="200px" :selected.sync="selected">
+      <g-slides-item name="第一张">
+        <div class="box">1</div>
+      </g-slides-item>
+      <g-slides-item name="第二张">
+        <div class="box">2</div>
+      </g-slides-item>
+      <g-slides-item name="第三张">
+        <div class="box">3</div>
+      </g-slides-item>
+    </g-slides>
   </div>
 </template>
 
 <script>
-import gButton from './button'
-import gCascader from "@/cascader";
-import db from './db'
-
-function ajax(parentId = 0){
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let result = db.filter(item => item.parent_id == parentId)
-      result.forEach(node => {
-        if(db.filter(item => item.parent_id === node.id).length > 0){
-          node.isLeaf = false
-        }else{
-          node.isLeaf = true
-        }
-      })
-      resolve(result)
-    }, 300)
-  })
-}
+import gSlides from '@/slides'
+import gSlidesItem from '@/slides-item'
 
 export default {
   name: "demo",
   components: {
-    gButton, gCascader
+    gSlides,gSlidesItem
   },
   data() {
     return {
-      selected: [],
-      source: []
-      //   [
-      //   {
-      //     name: '浙江',
-      //     children: [
-      //       {
-      //         name: '杭州',
-      //         children: [{name: '上城'}, {name: '下城'}, {name: '江干'},]
-      //       },
-      //       {
-      //         name: '嘉兴',
-      //         children: [{name: '南湖'}, {name: '秀洲'}, {name: '嘉善'},]
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     name: '福建',
-      //     children: [
-      //       {
-      //         name: '福州',
-      //         children: [{name: '鼓楼'}, {name: '台江'}, {name: '台山'},]
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     name: '合肥',
-      //     children: [{name: '瑶海'}, {name: '庐阳'}]
-      //   }
-      // ]
+      selected: '第三张'
     }
   },
-  created() {
-    ajax(0).then(result => {
-      this.source = result
-      console.log(result)
-    })
-  },
   methods:{
-    loadData(node, updateSource){
-      let {id} = node
-      ajax(id).then(result => {
-        updateSource(result)
-      })
-    },
-    // xxx(){
-    //   ajax(this.selected[0].id).then(result =>{
-    //     let lastLevelSelected = this.source.filter(item => item.id === this.selected[0].id)[0]
-    //     this.$set(lastLevelSelected, 'children', result)
-    //     console.log(lastLevelSelected)
-    //   })
-    // }
   }
 }
 </script>
 
-<style scoped>
-*{ margin: 0; padding: 0}
+<style>
+* { margin: 0; padding: 0}
 *, *::before, *::after{box-sizing: border-box}
+.wrapper{
+  margin: 40px;
+}
+.box{
+  width: 100%;
+  height: 300px;
+  background-color: #ddd;
+}
 </style>
---
+++ src/slides-item.vue
@@ -0,0 +1,60 @@
+<template>
+  <transition name="slide">
+    <div class="g-slides-item" v-if="visible" :class="{reverse}">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "GuluSlidesItem",
+  props:{
+    name:{
+      type: String,
+      required: true
+    }
+  },
+  computed:{
+    visible(){
+      return this.selected === this.name
+    }
+  },
+  data(){
+    return {
+      selected: undefined,
+      reverse: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.g-slides-item{ }
+.slide-leave-active{
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+.slide-enter-active, .slide-leave-active{
+  transition: all 1s;
+}
+.slide-enter{
+  transform: translateX(100%);
+  opacity: 0;
+}
+.slide-enter.reverse{
+  transform: translateX(-100%);
+  opacity: 0;
+}
+.slide-leave-to{
+  transform: translateX(-100%) scale(0.5);
+  opacity: 0;
+}
+.slide-leave-to.reverse{
+  transform: translateX(100%) scale(0.5);
+  opacity: 0;
+}
+</style>
